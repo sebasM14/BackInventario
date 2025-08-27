f368783477d0aefeaa5f1fffe7465dbbb9427cda
YeikooM <yeiki659@gmail.com>
2025-08-27 00:14:27 -0500

    Creacion del componente config configuracion de app.module y main ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConexionModule } from './config/conexion/conexion.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ConexionModule],
-  controllers: [AppController],
+   imports: [ConfigModule.forRoot({isGlobal: true,envFilePath:".env"}), ConexionModule ],
+  controllers: [AppController ],
   providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const puerto=Number(process.env.PUERTO_SERVIDOR);
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors();
+  await app.listen(puerto,()=>{
+    console.log(`Servidor funcionando puerto: `+puerto);
+  });
 }
+
+
 bootstrap();
